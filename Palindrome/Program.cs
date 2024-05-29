9f65d34083088b4876a21e4c9be202d8a1c48538
Hugo Silva <hugofigueira.dev@gmail.com>
2024-05-29 16:15:20 +0100

    Adding logic Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static bool Logic_Palindrome(string word)
+        {
+            if (word == null)
+            {
+                throw new ArgumentNullException("String cannot be null");
+            }
+
+            if (word.Length < 2)
+            {
+                return true;
+
+            }
+
+            return Logic_Palindrome(word);
+
+            bool IsPalindromeInside(string str)
+            {
+                int left = 0;
+                int right = str.Length - 1;
+
+                while (left < right)
+                {
+                    if (str[left] != str[right])
+                    {
+                        return false;
+                    }
+                    left++;
+                    right++;
+                }
+                return true;
+
+            }
+
+        }
+
+
+
+    }
+}
